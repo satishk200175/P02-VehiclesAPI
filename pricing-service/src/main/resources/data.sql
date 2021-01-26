50e1eabbf1a69dcd4f0c6e18ca2d473b8bb810fe
satishkumar <satishk20@outlook.com>
2021-01-25 22:37:35 -0500

    Convert to Microservice

---
+++ pricing-service/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO price (id, currency, price, vehicleid) VALUES (1, 'USD', 5000, 1);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (2, 'EUR', 6000, 2);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (3, 'IND', 7000, 3);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (4, 'EUR', 8000, 4);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (5, 'IND', 9000, 5);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (6, 'USD', 2000, 6);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (7, 'USD', 3000, 7);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (8, 'EUR', 4000, 8);
+INSERT INTO price (id, currency, price, vehicleid) VALUES (9, 'USD', 5600, 9);
